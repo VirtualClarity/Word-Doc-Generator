900ca201542afb4b5f97692e33580a51e69b1f3d
Ian Gibbs <ian.gibbs@virtualclarity.com>
2013-07-04 08:03:20 +0100

    Import of version at http://worddocgenerator.codeplex.com as of today

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Word-Doc-Generator
 ==================
 
-A local copy of worddocgenerator.codeplex.com - GPL software - send changes back to author
+We have used the software at worddocgenerator.codeplex.com in Hosting Designer. I had to make a change to make it fit our needs. I took a local copy of that software and placed in this repository to track my work. The change has been sent back to the author as a patch in line with the GPL under which it is licensed.
+
---
+++ WordDocumentGenerator.Client/Sample Document Generators/SampleDocumentWithTableAndChartsGenerator.cs
@@ -0,0 +1,70 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="SampleDocumentWithTableAndChartsGenerator.cs" author="Atul Verma">
+//     Copyright (c) Atul Verma. This utility along with samples demonstrate how to use the Open Xml 2.0 SDK and VS 2010 for document generation. They are unsupported, but you can use them as-is.
+// </copyright>
+// ------------------------------------------------------------------------
+
+namespace WordDocumentGenerator.Client
+{
+    using System.Linq;
+    using DocumentFormat.OpenXml.Drawing.Charts;
+    using DocumentFormat.OpenXml.Packaging;
+    using WordDocumentGenerator.Library;
+
+    /// <summary>
+    /// Document generator for template having table and charts
+    /// </summary>
+    public class SampleDocumentWithTableAndChartsGenerator : SampleDocumentWithTableGenerator
+    {
+        private readonly LineChartData lineChartData;
+        private readonly ScatterChartData scatterChartData;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleDocumentWithTableAndChartsGenerator"/> class.
+        /// </summary>
+        /// <param name="generationInfo">The generation info.</param>
+        /// <param name="lineChartData">The line chart data.</param>
+        /// <param name="scatterChartData">The scatter chart data.</param>
+        public SampleDocumentWithTableAndChartsGenerator(DocumentGenerationInfo generationInfo, LineChartData lineChartData, ScatterChartData scatterChartData)
+            : base(generationInfo)
+        {
+            this.lineChartData = lineChartData;
+            this.scatterChartData = scatterChartData;
+        }
+
+        /// <summary>
+        /// Refreshes the charts.
+        /// </summary>
+        /// <param name="mainDocumentPart">The main document part.</param>
+        protected override void RefreshCharts(MainDocumentPart mainDocumentPart)
+        {
+            if (mainDocumentPart != null)
+            {
+                foreach (ChartPart chartPart in mainDocumentPart.ChartParts)
+                {
+                    Chart chart = chartPart.ChartSpace.Elements<Chart>().FirstOrDefault();
+
+                    if (chart != null)
+                    {
+                        DocumentFormat.OpenXml.Drawing.Charts.ScatterChart scatterChart = chart.Descendants<DocumentFormat.OpenXml.Drawing.Charts.ScatterChart>().FirstOrDefault();
+                        DocumentFormat.OpenXml.Drawing.Charts.Line3DChart lineChart = chart.Descendants<DocumentFormat.OpenXml.Drawing.Charts.Line3DChart>().FirstOrDefault();
+
+                        if (scatterChart != null)
+                        {                            
+                            ScatterChartEx chartEx = new ScatterChartEx(chartPart, this.scatterChartData);
+                            chartEx.Refresh();
+                        }
+
+                        if (lineChart != null)
+                        {                            
+                            Line3DChartEx chartEx = new Line3DChartEx(chartPart, this.lineChartData);
+                            chartEx.Refresh();
+                        }
+                    }
+
+                    chartPart.ChartSpace.Save();
+                }
+            }
+        }
+    }
+}
